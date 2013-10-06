8f8eccda96ce93e052eb1be506f0d5a2ff26d5e7
benweet <benoit.schweblin@gmail.com>
2013-10-06 21:26:11 +0100

    Refactor resource location

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 app.use(express.compress());
-app.use('/stackedit', express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 3000);

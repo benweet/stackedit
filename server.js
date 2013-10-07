f1f21df46da806a68d6f64504abb61de418b0230
benweet <benoit.schweblin@gmail.com>
2013-10-07 02:11:56 +0100

    Force https on stackedit.io

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var app = express();
+app.all('*', function(req, res, next) {
+    console.log(req.headers);
+    if (req.headers.host == 'stackedit.io' && req.headers['x-forwarded-proto'] != 'https') {
+        res.redirect('https://stackedit.io' + req.url);
+    }
+    else {
+        next();
+    }
+});
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 3000);

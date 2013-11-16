03192b6f4152a0888a3fef28d9bd623cd9d441ef
benweet <benoit.schweblin@gmail.com>
2013-11-16 18:09:33 +0000

    Implemented css working indicator

--- public/res/extensions/workingIndicator.js
+++ public/res/extensions/workingIndicator.js
@@ -2,20 +2,41 @@ define([
     "jquery",
     "underscore",
     "classes/Extension",
-], function($, _, Extension) {
+    "crel"
+], function ($, _, Extension, crel) {
 
     var workingIndicator = new Extension("workingIndicator", "Working Indicator");
 
     var $bodyElt;
     var $workingIndicatorElt;
-    workingIndicator.onAsyncRunning = function(isRunning) {
+    var intervalId;
+    workingIndicator.onAsyncRunning = function (isRunning) {
         $bodyElt.toggleClass("working", isRunning);
         $workingIndicatorElt.toggleClass("show", isRunning);
+        if(isRunning) {
+            animate();
+            intervalId = setInterval(animate, 200);
+        }
+        else {
+            clearInterval(intervalId);
+        }
     };
     
-    workingIndicator.onReady = function() {
+    var indicatorElts = [];
+    var loop = 0;
+    function animate() {
+        indicatorElts[loop].className = '';
+        loop = (loop + 1) % 3;
+        indicatorElts[loop].className = 'highlighted';
+    }
+
+    workingIndicator.onReady = function () {
         $bodyElt = $(document.body);
         $workingIndicatorElt = $(".working-indicator");
+        for (var i = 0; i < 3; i++) {
+            indicatorElts.push(crel('div'));
+        }
+        $workingIndicatorElt.append(indicatorElts);
     };
 
     return workingIndicator;

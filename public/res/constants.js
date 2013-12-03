95caf6674f74aeb385ed618d4c95366ab841fb37
benweet <benoit.schweblin@gmail.com>
2013-12-03 23:11:58 +0000

    Release v3.0.1

--- public/res/constants.js
+++ public/res/constants.js
@@ -1,6 +1,6 @@
 define([], function() {
     var constants = {};
-    constants.VERSION = "3.0.0";
+    constants.VERSION = "3.0.1";
     
     constants.MAIN_URL = "https://stackedit.io/";
     constants.GOOGLE_ANALYTICS_ACCOUNT_ID = "UA-39556145-1";

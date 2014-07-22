134fddc4aaa2e4bd4524ae5aa2d9737b4368b344
benweet <benoit.schweblin@gmail.com>
2014-07-22 12:42:21 +0100

    Simplified GitHub publish dialog

--- public/res/providers/githubProvider.js
+++ public/res/providers/githubProvider.js
@@ -19,13 +19,17 @@ define([
 
     githubProvider.newPublishAttributes = function(event) {
         var publishAttributes = {};
-        publishAttributes.repository = utils.getInputTextValue("#input-publish-github-reponame", event);
-        publishAttributes.username = utils.getInputTextValue("#input-publish-github-username");
+        publishAttributes.repository = utils.getInputTextValue("#input-publish-github-repo", event);
         publishAttributes.branch = utils.getInputTextValue("#input-publish-github-branch", event);
         publishAttributes.path = utils.getInputTextValue("#input-publish-file-path", event);
         if(event.isPropagationStopped()) {
             return undefined;
         }
+		var parsedRepository = publishAttributes.repository.match(/[\/:]?([^\/:]+)\/([^\/]+?)(?:\.git)?$/);
+		if(parsedRepository) {
+			publishAttributes.repository = parsedRepository[2];
+			publishAttributes.username = parsedRepository[1];
+		}
         return publishAttributes;
     };
 

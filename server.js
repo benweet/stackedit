3e061019eaedc75790852839caa02aa03cc5071e
benweet <benoit.schweblin@gmail.com>
2014-08-20 00:26:01 +0100

    Enabled cluster mode

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+var cluster = require('cluster');
 var app = require('./app');
 
-// Listen on port 3000
-var port = process.env.PORT || 3000;
-app.listen(port, null, function() {
-	console.log('Server started: http://localhost:' + port);
-});
+var count = require('os').cpus().length;
+
+if(!process.env.NO_CLUSTER && cluster.isMaster) {
+	for(var i = 0; i < count; i++) {
+		cluster.fork();
+	}
+	cluster.on('exit', function(worker) {
+		console.log('Worker died. Spawning a new process...');
+		cluster.fork();
+	});
+}
+else {
+	// Listen on port 3000
+	var port = process.env.PORT || 3000;
+	app.listen(port, null, function() {
+		console.log('Server started: http://localhost:' + port);
+	});
+}
+

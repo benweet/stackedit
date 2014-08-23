f7820adc200d23fbdbd0119fcbc299757b0fceb4
benweet <benoit.schweblin@gmail.com>
2014-08-24 00:57:37 +0100

    Release v4.0.2

--- public/res/constants.js
+++ public/res/constants.js
@@ -1,6 +1,6 @@
 define([], function() {
 	var constants = {};
-	constants.VERSION = "4.0.1";
+	constants.VERSION = "4.0.2";
 	constants.MAIN_URL = "https://stackedit.io/";
 	constants.GOOGLE_ANALYTICS_ACCOUNT_ID = "UA-39556145-1";
 	constants.GOOGLE_API_KEY = "AIzaSyAeCU8CGcSkn0z9js6iocHuPBX4f_mMWkw";

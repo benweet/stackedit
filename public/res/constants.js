75f9c9baf92d76c26b17357c24a8768f1154c1ea
Jesper Rønn-Jensen <jesperrr@gmail.com>
2014-12-08 14:02:34 +0100

    removed site-specific configuration...we will not care about other websites here

--- public/res/constants.js
+++ public/res/constants.js
@@ -36,38 +36,6 @@ define([], function() {
 	constants.WORDPRESS_CLIENT_ID = '23361';
 	constants.WORDPRESS_PROXY_URL = "https://stackedit-io-wordpress-proxy.herokuapp.com/";
 
-	if(location.hostname.indexOf("stackedit.io") === 0) {
-		constants.BASE_URL = constants.MAIN_URL;
-		constants.GOOGLE_CLIENT_ID = '241271498917-t4t7d07qis7oc0ahaskbif3ft6tk63cd.apps.googleusercontent.com';
-		constants.GITHUB_CLIENT_ID = '710fc67886ab1ae8fee6';
-		constants.GATEKEEPER_URL = "https://stackedit-io-gatekeeper.herokuapp.com/";
-		constants.TUMBLR_PROXY_URL = "https://stackedit-io-tumblr-proxy.herokuapp.com/";
-	}
-	else if(location.hostname.indexOf("benweet.github.io") === 0) {
-		constants.BASE_URL = 'http://benweet.github.io/stackedit/';
-		constants.GOOGLE_CLIENT_ID = '241271498917-jpto9lls9fqnem1e4h6ppds9uob8rpvu.apps.googleusercontent.com';
-		constants.GITHUB_CLIENT_ID = 'fa0d09514da8377ee32e';
-		constants.GATEKEEPER_URL = "https://stackedit-gatekeeper.herokuapp.com/";
-		constants.TUMBLR_PROXY_URL = "https://stackedit-tumblr-proxy.herokuapp.com/";
-		constants.WORDPRESS_CLIENT_ID = '3185';
-		constants.WORDPRESS_PROXY_URL = "https://stackedit-wordpress-proxy.herokuapp.com/";
-	}
-	else if(location.hostname.indexOf("stackedit-beta.herokuapp.com") === 0) {
-		constants.BASE_URL = 'https://stackedit-beta.herokuapp.com/';
-		constants.GOOGLE_CLIENT_ID = '241271498917-9bbplknkt0ljv5gaudhoiogp13hd18be.apps.googleusercontent.com';
-		constants.GITHUB_CLIENT_ID = 'e9034ae191c3a8a1c5ed';
-		constants.GATEKEEPER_URL = "https://stackedit-beta-gatekeeper.herokuapp.com/";
-		constants.TUMBLR_PROXY_URL = "https://stackedit-beta-tumblr-proxy.herokuapp.com/";
-		constants.WORDPRESS_CLIENT_ID = '34786';
-		constants.WORDPRESS_PROXY_URL = "https://stackedit-beta-wordpress-proxy.herokuapp.com/";
-	}
-	else if(location.hostname.indexOf("benweet.insomnia247.nl") === 0) {
-		constants.BASE_URL = "http://benweet.insomnia247.nl/stackedit/";
-		constants.GOOGLE_CLIENT_ID = '241271498917-52hae7a08hv7ltenv7km8h7lghno9sk3.apps.googleusercontent.com';
-		constants.GITHUB_CLIENT_ID = 'd2943d6074b2d9c4a830';
-		constants.GATEKEEPER_URL = "https://stackedit-gatekeeper-insomnia.herokuapp.com/";
-		constants.TUMBLR_PROXY_URL = "https://stackedit-tumblr-proxy-beta.herokuapp.com/";
-	}
 
 	constants.THEME_LIST = {
 		"blue": "Blue",

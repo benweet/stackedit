ee54ea08da2065c9a67463a864433dcb96d2647e
benweet <benoit.schweblin@gmail.com>
2014-12-16 01:05:23 +0000

    Prepare release

--- server.js
+++ server.js
@@ -15,24 +15,24 @@ if(!process.env.NO_CLUSTER && cluster.isMaster) {
 	});
 }
 else {
-	var server;
 	var port = process.env.PORT || 3000;
 	if(port == 443) {
-		// Configure HTTPS on OpsWorks
+		// OpsWorks configuration
 		var fs = require('fs');
 		var credentials = {
 			key: fs.readFileSync(__dirname + '/../../shared/config/ssl.key', 'utf8'),
 			cert: fs.readFileSync(__dirname + '/../../shared/config/ssl.crt', 'utf8'),
 			ca: fs.readFileSync(__dirname + '/../../shared/config/ssl.ca', 'utf8').split('\n\n')
 		};
-		server = https.createServer(credentials, app);
+		var httpsServer = https.createServer(credentials, app);
+		httpsServer.listen(port, null, function() {
+			console.log('HTTPS server started: https://localhost');
+		});
+		port = 80;
 	}
-	else {
-		server = http.createServer(app);
-	}
-
-	server.listen(port, null, function() {
-		console.log('Server started: http://localhost:' + port);
+	var httpServer = http.createServer(app);
+	httpServer.listen(port, null, function() {
+		console.log('HTTP server started: http://localhost:' + port);
 	});
 }
 

93bbd7d971338976c114ad8dfa77d8fe853d3641
benweet <benoit.schweblin@gmail.com>
2018-02-01 23:24:06 +0000

    Fixed pdf template

--- public/res/settings.js
+++ public/res/settings.js
@@ -39,11 +39,11 @@ define([
 			'<head>',
 			'<meta charset="utf-8">',
 			'<title><%= documentTitle %></title>',
-			'<link rel="stylesheet" href="http://localhost/res-min/themes/base.css" />',
+			'<link rel="stylesheet" href="https://stackedit.io/res-min/themes/base.css" />',
 			'<script type="text/x-mathjax-config">',
 			'MathJax.Hub.Config({ messageStyle: "none" });',
 			'</script>',
-			'<script type="text/javascript" src="http://localhost/res/bower-libs/MathJax/MathJax.js?config=TeX-AMS_HTML"></script>',
+			'<script type="text/javascript" src="https://stackedit.io/res/bower-libs/MathJax/MathJax.js?config=TeX-AMS_HTML"></script>',
 			'</head>',
 			'<body><%= documentHTML %></body>',
 			'</html>'

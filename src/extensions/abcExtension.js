7b623cc77ff895ad7df4669d54c05a0280f404e6
Felix Wu <mail@flxwu.com>
2018-08-21 11:06:37 +0200

    remove plugin import

--- src/extensions/abcExtension.js
+++ src/extensions/abcExtension.js
@@ -1,13 +1,11 @@
-/* eslint-disable */
 import abcjs from 'abcjs';
-import markdownItNotesSheet from './libs/markdownItNotesSheet';
 import extensionSvc from '../services/extensionSvc';
 
 const render = (elt) => {
   const abcContent = elt.textContent;
   elt.parentNode.parentNode.id = 'abcSheetPaper';
   abcjs.renderAbc('abcSheetPaper', abcContent, {});
-}
+};
 
 extensionSvc.onGetOptions((options, properties) => {
   options.abc = properties.extensions.abc.enabled;

f55f2a2b2ac1c21493a9b35a425b240e61440547
Daniel Graziani <59689758+danielgraziani91@users.noreply.github.com>
2021-04-14 11:31:37 +0200

    Update GithubOpenModal.vue

--- src/components/modals/providers/GithubOpenModal.vue
+++ src/components/modals/providers/GithubOpenModal.vue
@@ -20,7 +20,7 @@
       <form-entry label="Branch" info="optional">
         <input slot="field" class="textfield" type="text" v-model.trim="branch" @keydown.enter="resolve()">
         <div class="form-entry__info">
-          If not supplied, the <code>master</code> branch will be used.
+          If not supplied, the <code>main</code> branch will be used.
         </div>
       </form-entry>
     </div>
